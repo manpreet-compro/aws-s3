3d0fff327ec7f3a7cf6d23938242af841c04a94e
Manpreet <manpreet.singh@comprotechnologies.com>
2019-12-03 17:43:39 +0530

    Remove redundant code

--- helpers/aws.js
+++ helpers/aws.js
@@ -38,25 +38,3 @@ module.exports = {
     listData,
     getData
 }
-
-
-// File that needs to be uploaded.
-// var file = '/tmp/photos-europe.tar'
-
-// // Make a bucket called europetrip.
-// minioClient.makeBucket('europetrip', 'us-east-1', function(err) {
-//     if (err) return console.log(err)
-
-//     console.log('Bucket created successfully in "us-east-1".')
-
-//     var metaData = {
-//         'Content-Type': 'application/octet-stream',
-//         'X-Amz-Meta-Testing': 1234,
-//         'example': 5678
-//     }
-//     // Using fPutObject API upload your file to the bucket europetrip.
-//     minioClient.fPutObject('europetrip', 'photos-europe.tar', file, metaData, function(err, etag) {
-//       if (err) return console.log(err)
-//       console.log('File uploaded successfully.')
-//     });
-// });
